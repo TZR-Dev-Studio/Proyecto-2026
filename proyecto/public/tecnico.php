c9c4817384c872e44dbd2edeedade2fc044fb832
TZRDevstudio <tzr.devstudio@proton.me>
2026-07-20 20:35:15 -0300

    feat: agregar panel tecnico con control de acceso

---
+++ proyecto/public/tecnico.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if (!isset($_SESSION["usuario"])) {
+    $mensaje = "Acceso Denegado: Sesión no iniciada";
+    header("Location: login.php?" . "error=" . $mensaje);
+    exit;
+}
+if (!isset($_SESSION["tecnico"]) || $_SESSION["tecnico"] !== true ) {
+    $mensaje = "Acceso Denegado: Rol incorrecto";
+    header("Location: login.php?" . "error=" . $mensaje);
+    exit;
+}
+require_once __DIR__ . "/../app/vista/tecnico.php";
+?>
